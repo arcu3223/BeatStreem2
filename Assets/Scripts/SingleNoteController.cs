04757bbeef567a099234872aee7bf91e6c72f308
arcu3223 <kakoyudai@yahoo.co.jp>
2020-08-06 22:12:33 +0900

    BeatStreem2作成

---
+++ Assets/Scripts/SingleNoteController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleNoteController : NoteControllerBase
+{
+    void Update()
+    {
+        // ノーツの座標
+        Vector2 position = new Vector2();
+        position.x = (noteProperty.beatBegin - PlayerController.CurrentBeat) *
+       PlayerController.ScrollSpeed;
+        transform.localPosition = position;
+        position.y = noteProperty.lane - 2;
+    }
+}
