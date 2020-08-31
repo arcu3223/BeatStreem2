f9282d35d8794d3fe72e5fe28b8394b56ad5748a
arcu3223 <kakoyudai@yahoo.co.jp>
2020-08-31 23:55:41 +0900

    レーン0軸の時のロングノーツの設定

--- Assets/Scripts/LongNoteController.cs
+++ Assets/Scripts/LongNoteController.cs
@@ -32,16 +32,20 @@ public class LongNoteController : NoteControllerBase
     {
         // 始点の座標（beatBeginによる指定）
         Vector2 positionBegin = new Vector2();
+        positionBegin.x = (noteProperty.beatBegin - PlayerController.CurrentBeat) *
+     PlayerController.ScrollSpeed;
+        positionBegin.y = 0;
         objBegin.transform.localPosition = positionBegin;
 
         // 終点の座標（beatEndによる指定）
         Vector2 positionEnd = new Vector2();
+        positionEnd.x = (noteProperty.beatEnd - PlayerController.CurrentBeat) *
+            PlayerController.ScrollSpeed;
+        positionEnd.y = 0;
         objEnd.transform.localPosition = positionEnd;
 
         // ノーツの座標
-        if (objBegin)
-        {
-            float present_Location = (noteProperty.beatBegin - PlayerController.CurrentBeat) *
+        float present_Location = (noteProperty.beatBegin - PlayerController.CurrentBeat) *
             PlayerController.ScrollSpeed;
 
             // オブジェクトの移動
@@ -119,97 +123,15 @@ public class LongNoteController : NoteControllerBase
             {
                 transform.position = Vector2.Lerp(StartLine[14].position, GoalLine[14].position, present_Location);
             }
-        }
-
-        if (objEnd)
-        {
-            float present_Location = (noteProperty.beatEnd - PlayerController.CurrentBeat) *
-            PlayerController.ScrollSpeed;
-
-            // オブジェクトの移動
-            if (this.noteProperty.lane == 0)
-            {
-                transform.position = Vector2.Lerp(StartLine[0].position, GoalLine[0].position, present_Location);
-            }
-
-            else if (this.noteProperty.lane == 1)
-            {
-                transform.position = Vector2.Lerp(StartLine[1].position, GoalLine[1].position, present_Location);
-            }
-
-            else if (this.noteProperty.lane == 2)
-            {
-                transform.position = Vector2.Lerp(StartLine[2].position, GoalLine[2].position, present_Location);
-            }
-
-            else if (this.noteProperty.lane == 3)
-            {
-                transform.position = Vector2.Lerp(StartLine[3].position, GoalLine[3].position, present_Location);
-            }
-
-            else if (this.noteProperty.lane == 4)
-            {
-                transform.position = Vector2.Lerp(StartLine[4].position, GoalLine[4].position, present_Location);
-            }
-
-            else if (this.noteProperty.lane == 5)
-            {
-                transform.position = Vector2.Lerp(StartLine[5].position, GoalLine[5].position, present_Location);
-            }
-
-            else if (this.noteProperty.lane == 6)
-            {
-                transform.position = Vector2.Lerp(StartLine[6].position, GoalLine[6].position, present_Location);
-            }
-
-            else if (this.noteProperty.lane == 7)
-            {
-                transform.position = Vector2.Lerp(StartLine[7].position, GoalLine[7].position, present_Location);
-            }
-
-            else if (this.noteProperty.lane == 8)
-            {
-                transform.position = Vector2.Lerp(StartLine[8].position, GoalLine[8].position, present_Location);
-            }
-
-            else if (this.noteProperty.lane == 9)
-            {
-                transform.position = Vector2.Lerp(StartLine[9].position, GoalLine[9].position, present_Location);
-            }
-
-            else if (this.noteProperty.lane == 10)
-            {
-                transform.position = Vector2.Lerp(StartLine[10].position, GoalLine[10].position, present_Location);
-            }
-
-            else if (this.noteProperty.lane == 11)
-            {
-                transform.position = Vector2.Lerp(StartLine[11].position, GoalLine[11].position, present_Location);
-            }
-
-            else if (this.noteProperty.lane == 12)
-            {
-                transform.position = Vector2.Lerp(StartLine[12].position, GoalLine[12].position, present_Location);
-            }
-
-            else if (this.noteProperty.lane == 13)
-            {
-                transform.position = Vector2.Lerp(StartLine[13].position, GoalLine[13].position, present_Location);
-            }
-
-            else if (this.noteProperty.lane == 14)
-            {
-                transform.position = Vector2.Lerp(StartLine[14].position, GoalLine[14].position, present_Location);
-            }
-        }
 
         // 軌跡部分の座標は始点と終点の中心に設定
         Vector2 positionTrail = (positionBegin + positionEnd) / 2f;
         objTrail.transform.localPosition = positionTrail;
 
-        // 軌跡部分のY拡大率は終点の始点の座標の差に設定
+        // 軌跡部分の拡大率は終点の始点の座標の差に設定
         Vector2 scale = objTrail.transform.localScale;
         scale.x = positionEnd.x - positionBegin.x;
+        //scale.y = positionEnd.y - positionBegin.y;
         objTrail.transform.localScale = scale;
     }
 
